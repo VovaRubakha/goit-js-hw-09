ffcfbc19097f4249d9c0c4a67153a9bd128a8371
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-04-26 21:07:42 +0300

    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+ref = {
+  startBtn: document.querySelector(`button[data-start]`),
+  stopBtn: document.querySelector(`button[data-stop]`),
+};
+let intervalIl;
+function onStartChangeColor() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  setDisable(ref.startBtn);
+  removeDisable(ref.stopBtn);
+}
 
+function onStopChangeColor() {
+  clearInterval(intervalId);
+  setDisable(ref.stopBtn);
+  removeDisable(ref.startBtn);
+}
+
+function setDisable(e) {
+  e.setAttribute(`disabled`, `disabled`);
+}
+
+function removeDisable(e) {
+  e.removeAttribute(`disabled`);
+}
+
+ref.startBtn.addEventListener(`click`, onStartChangeColor);
+ref.stopBtn.addEventListener(`click`, onStopChangeColor);
