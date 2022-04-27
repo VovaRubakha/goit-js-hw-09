3f73861d99fc64f21fd1164831f4d53a8f3c9ebd
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-04-27 20:59:33 +0300

    Done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const formRef = document.querySelector(`.form`);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = Number(formRef.elements.delay.value);
+  const step = Number(formRef.elements.step.value);
+  const amount = Number(formRef.elements.amount.value);
+  let counter = 1;
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(counter, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    counter += 1;
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay);
+  });
+}
+
+formRef.addEventListener('submit', onSubmit);
